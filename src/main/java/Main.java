aed1828a3b4072423f407c8bb2cc4ba4bb803466
viuknaite <viuknaite@gmail.com>
2020-06-29 17:34:43 +0300

    test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,15 @@
-public class Main {
-    public static void main(String[] args){
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
+public class Main {
+    static WebDriver chromeDriver;
+    static WebDriver firefoxDriver;
+    public static void main(String[] args){
+        System.setProperty("webdriver.chrome.driver","C:\\Users\\viuknaite\\IdeaProjects\\IMDbTestSelenium\\drivers\\chromedriver.exe");
+        System.setProperty("webdriver.gecko.driver","C:\\Users\\viuknaite\\IdeaProjects\\IMDbTestSelenium\\drivers\\geckodriver.exe");
+
+        chromeDriver = new ChromeDriver();
+        firefoxDriver = new FirefoxDriver();
     }
 }
